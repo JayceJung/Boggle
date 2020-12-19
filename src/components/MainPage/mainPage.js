b0e3c3c73f825e52eae6350a7b68055f04ce2242
Jayce <jy4jung@uwaterloo.ca>
2020-12-20 06:23:32 +0900

    deleted and made new folders

---
+++ src/components/MainPage/mainPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function mainPage() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies
+      sagittis ultrices. Nam eu arcu commodo, vehicula ex ut, convallis mi.
+      Quisque arcu metus, semper vel hendrerit vel, facilisis a metus. Morbi
+      elementum ante eu elit molestie lacinia. Curabitur tempus metus id urna
+      sagittis, ac consequat risus molestie. Donec nec ullamcorper enim. Cras
+      sed sem risus. Quisque pellentesque neque sapien, eu faucibus neque
+      porttitor eu. Vivamus semper purus eu tristique accumsan. Mauris ac lorem
+      at nibh mattis dictum. Cras scelerisque sagittis tortor non molestie.
+      Mauris et mauris eget urna posuere suscipit.
+    </p>
+  );
+}
+
