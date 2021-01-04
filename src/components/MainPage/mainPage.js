f937890335ffb5136f5f695ebbc2a05690c913a7
Jayce <jy4jung@uwaterloo.ca>
2021-01-04 12:14:02 +0900

    added how to play modal

--- src/components/MainPage/mainPage.js
+++ src/components/MainPage/mainPage.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Board from '../Board/board';
 import RuleModal from '../RuleModal/ruleModal';
+import Modal from 'react-modal';
 import WordList from '../WordList/wordList';
 import TimerButton from '../TimerButton/timerButton';
+import Button from '@material-ui/core/Button';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './mainPage.css';
 
 const boggle = require('pf-boggle');
+const customStyles = {
+    content: {
+        width: '70%',
+        padding: '1% 3%',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 export default function MainPage(props) {
     const allAnswer = boggle.solve(props.boggleArray);
     const boggleAnswer = allAnswer.filter((words) => words.word.length > 2);
-
     const [gameStatus, setGameStatus] = useState('init');
+    const [modalIsOpen, setIsOpen] = useState(false);
 
     const startGame = () => {
         setGameStatus('started');
@@ -23,11 +37,31 @@ export default function MainPage(props) {
         setGameStatus('done');
     };
 
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
     return (
         <div id="mainWrap">
             <Row id="title" center="lg">
-                BOGGLE
+                <Col xs={3}></Col>
+                <Col xs={6}>BOGGLE</Col>
+                <Col xs={3} id="help">
+                    <Button onClick={openModal}>How to play</Button>
+                </Col>
             </Row>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <RuleModal />
+            </Modal>
             <Row id="gameWrap">
                 <Col xs={8}>
                     <Board array={props.boggleArray} gameStatus={gameStatus} />
