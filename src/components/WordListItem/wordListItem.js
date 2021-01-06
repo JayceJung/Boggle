03395cee157c0527c2abeb105181e00638c8fe9e
Zain Noor <zainnoor516@gmail.com>
2021-01-06 18:40:54 -0500

    adds point system

--- src/components/WordListItem/wordListItem.js
+++ src/components/WordListItem/wordListItem.js
@@ -4,16 +4,54 @@ import { useSelector } from 'react-redux';
 export default function WordListItem(props) {
     const words = useSelector((state) => state.words);
     let listItems;
+    let tally = 0;
     if (props.answers) {
         const boggleAnswer = props.answers.map((answer) => answer.word);
         listItems = words.map((word) => {
+            let points;
             if (boggleAnswer.includes(word)) {
-                return <li key={word}>{word}</li>;
-            } else {
+                if (word.length <= 4) {
+                    points = 1;
+                } else if (word.length === 5) {
+                    points = 2;
+                } else if (word.length === 6) {
+                    points = 3;
+                } else if (word.length === 7) {
+                    points = 5;
+                } else if (word.length >= 8) {
+                    points = 11;
+                }
+
+                tally += points;
+
                 return (
-                    <li className="incorrect" key={word}>
-                        {word}
-                    </li>
+                    <div>
+                        <li key={word}>{word}</li>
+                        <p>{points}</p>
+                    </div>
+                );
+            } else {
+                if (word.length <= 4) {
+                    points = -1;
+                } else if (word.length === 5) {
+                    points = -2;
+                } else if (word.length === 6) {
+                    points = -3;
+                } else if (word.length === 7) {
+                    points = -5;
+                } else if (word.length >= 8) {
+                    points = -11;
+                }
+
+                tally += points;
+
+                return (
+                    <div>
+                        <li className="incorrect" key={word}>
+                            {word}
+                        </li>
+                        <p>{points}</p>
+                    </div>
                 );
             }
         });
@@ -24,6 +62,7 @@ export default function WordListItem(props) {
     return (
         <div>
             <ul>{listItems}</ul>
+            {props.answers ? <p>score: {tally}</p> : null}
         </div>
     );
 }
