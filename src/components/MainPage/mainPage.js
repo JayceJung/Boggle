4e3e0ca462fad276dae9cb12a18f2ab3a71fe992
Jayce <jy4jung@uwaterloo.ca>
2021-01-07 09:49:50 +0900

    viewBoard completed

--- src/components/MainPage/mainPage.js
+++ src/components/MainPage/mainPage.js
@@ -50,7 +50,6 @@ export default function MainPage(props) {
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 style={customStyles}
-                contentLabel="Example Modal"
             >
                 <RuleModal />
             </Modal>
--- src/components/SecondPage/secondPage.js
+++ src/components/SecondPage/secondPage.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Button from '@material-ui/core/Button';
+import Board from '../Board/board';
 import WordList from '../WordList/wordList';
+import Modal from 'react-modal';
 import { useDispatch } from 'react-redux';
 import { initGame, resetWordsList } from '../../actions';
+
 import './secondPage.css';
 const boggle = require('pf-boggle');
+const customStyles = {
+    content: {
+        width: '70%',
+        padding: '1% 3%',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 export default function SecondPage(props) {
+    const [modalIsOpen, setIsOpen] = useState(false);
     const dispatch = useDispatch();
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    
+    const closeModal = () => {
+        setIsOpen(false);
+    };
 
     const newGame = () => {
         dispatch(resetWordsList());
@@ -38,9 +62,16 @@ export default function SecondPage(props) {
                     />
                 </Col>
             </Row>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+            >
+                <Board array={props.boggleArray} />
+            </Modal>
             <Row id="answers" center="lg">
                 <Col xs={4}>
-                    <Button id="viewBoard">ViewBoard</Button>
+                    <Button id="viewBoard" onClick={openModal}>ViewBoard</Button>
                 </Col>
             </Row>
         </div>
